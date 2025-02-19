a5e800710556c03f0a0271de4cb1ddbe24f89be9
Dan_foodz <131561731+DanielTWinn@users.noreply.github.com>
2025-02-19 17:23:09 -0600

    testing

--- apriltagtool/main.js
+++ apriltagtool/main.js
@@ -1,5 +1,13 @@
 if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
     console.log("Let's get this party started")
 }
+const cameraVideoStream = document.getElementById('camera-stream')
 
-navigator.mediaDevices.getUserMedia({video: true})
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia({ video: true })) {
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then ((stream) => {
+              cameraVideoStream.srcObject = stream
+              cameraVideoStream.play()
+      })
+}
