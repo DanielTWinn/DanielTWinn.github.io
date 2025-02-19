4a1ee5fba1cff07c90a28d70966c30480ebd00a3
Dan_foodz <131561731+DanielTWinn@users.noreply.github.com>
2025-02-19 17:38:14 -0600

    test

--- apriltagtool/main.js
+++ apriltagtool/main.js
@@ -5,7 +5,7 @@ const cameraVideoStream = document.getElementById('camera-stream')
 
 if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia({ video: true })) {
     navigator.mediaDevices
-      .getUserMedia({ video: true })
+      .getUserMedia({ video: {facingMode: "environment"} })
       .then ((stream) => {
               cameraVideoStream.srcObject = stream
               cameraVideoStream.play()
